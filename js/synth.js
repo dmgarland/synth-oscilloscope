d95d6cce366f2b76f159cb21924c0bb6978d7db5
Dan Garland <dan@dangarland.co.uk>
2022-03-05 10:05:17 +0000

    Oscilloscope

--- js/synth.js
+++ js/synth.js
@@ -10,7 +10,7 @@ var total_samples = audioCtx.sampleRate * seconds;
 var myArrayBuffer = audioCtx.createBuffer(channels.mono, total_samples, audioCtx.sampleRate);
 console.log(audioCtx.sampleRate);
 
-var ref_freq = 440.0; // concert pitch
+var ref_freq = 440.0; // concert pitch 440 cycles (hertz) per second
 var A = Math.pow(1 / 12, 2); // equal temperament
 var steps = 0; // how many ET semitones away from concert pitch?
 var pulse_hz = ref_freq * Math.pow(A, steps);
@@ -20,14 +20,59 @@ var TAU = Math.PI * 2; // ratio between circumfrence and radius
 
 for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
     fillBuffer(myArrayBuffer.getChannelData(channel));
+    plotOscilloscope(myArrayBuffer.getChannelData(channel).slice(0, 1000));
 }
 
 // PCM = Pulse code modulation https://en.wikipedia.org/wiki/Pulse-code_modulation
 function fillBuffer(pcm_data) {
     for (var i = 0; i < pcm_data.length; i++) {
         pcm_data[i] = Math.sin(phase * TAU);
-        phase = (phase + increment) % 1.0
+        phase = (phase + increment) % 1.0;
     }
+}
+
+function plotOscilloscope(pcm_data) {
+    var ctx = document.getElementById('oscilloscope').getContext('2d');
+    var labels = Array.from(pcm_data.keys());
+    var data = {
+        labels: labels,
+        datasets: [{
+            data: pcm_data
+        }]
+    };
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            elements: {
+                point: {
+                    radius: 0
+                },
+                line: {
+                    borderColor: '#32b832'
+                }
+            },
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Samples (Time)'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Amplitude'
+                    }
+                }
+            }
+        }
+    });
 }
 
 // Get an AudioBufferSourceNode.
