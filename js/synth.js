4a41d15cd20138ce38004efbcc6e2cea057836d4
Dan Garland <dan@dangarland.co.uk>
2022-03-05 11:02:34 +0000

    Update graph with steps

--- js/synth.js
+++ js/synth.js
@@ -8,39 +8,45 @@ var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var seconds = 1;
 var total_samples = audioCtx.sampleRate * seconds;
 var myArrayBuffer = audioCtx.createBuffer(channels.mono, total_samples, audioCtx.sampleRate);
-console.log(audioCtx.sampleRate);
-
 var ref_freq = 440.0; // concert pitch 440 cycles (hertz) per second
-var A = Math.pow(1 / 12, 2); // equal temperament
+var A = Math.pow(2, 1 / 12); // equal temperament
 var steps = 0; // how many ET semitones away from concert pitch?
-var pulse_hz = ref_freq * Math.pow(A, steps);
-var increment = pulse_hz / audioCtx.sampleRate;
 var phase = 0.0; // How far along the wave we are?
 var TAU = Math.PI * 2; // ratio between circumfrence and radius
+var chart;
 
-for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
-    fillBuffer(myArrayBuffer.getChannelData(channel));
-    plotOscilloscope(myArrayBuffer.getChannelData(channel).slice(0, 1000));
+function update() {
+    phase = 0.0
+    steps = document.getElementById('steps').value;
+
+    for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
+        fillBuffer(myArrayBuffer.getChannelData(channel));
+        plotOscilloscope(myArrayBuffer.getChannelData(channel).slice(0, 101));
+    }
+    sound();
 }
+
 
 // PCM = Pulse code modulation https://en.wikipedia.org/wiki/Pulse-code_modulation
 function fillBuffer(pcm_data) {
+    var pulse_hz = ref_freq * Math.pow(A, steps);
+    var increment = pulse_hz / audioCtx.sampleRate;
+
     for (var i = 0; i < pcm_data.length; i++) {
         pcm_data[i] = Math.sin(phase * TAU);
         phase = (phase + increment) % 1.0;
     }
 }
 
-function plotOscilloscope(pcm_data) {
+function initOscilloscope() {
     var ctx = document.getElementById('oscilloscope').getContext('2d');
-    var labels = Array.from(pcm_data.keys());
     var data = {
-        labels: labels,
+        labels: [],
         datasets: [{
-            data: pcm_data
+            data: []
         }]
     };
-    var chart = new Chart(ctx, {
+    chart = new Chart(ctx, {
         type: 'line',
         data: data,
         options: {
@@ -75,16 +81,31 @@ function plotOscilloscope(pcm_data) {
     });
 }
 
-// Get an AudioBufferSourceNode.
-// This is the AudioNode to use when we want to play an AudioBuffer
-var source = audioCtx.createBufferSource();
+function plotOscilloscope(pcm_data) {
+    chart.data.labels = Array.from(pcm_data.keys());
+    chart.data.datasets = [
+        {
+            data: pcm_data
+        }
+    ]
+    chart.update()
+}
 
-// set the buffer in the AudioBufferSourceNode
-source.buffer = myArrayBuffer;
+function sound() {
+    // Get an AudioBufferSourceNode.
+    // This is the AudioNode to use when we want to play an AudioBuffer
+    var source = audioCtx.createBufferSource();
 
-// connect the AudioBufferSourceNode to the
-// destination so we can hear the sound
-source.connect(audioCtx.destination);
+    // set the buffer in the AudioBufferSourceNode
+    source.buffer = myArrayBuffer;
 
-// start the source playing
-source.start();
+    // connect the AudioBufferSourceNode to the
+    // destination so we can hear the sound
+    source.connect(audioCtx.destination);
+
+    // start the source playing
+    source.start();
+}
+
+initOscilloscope();
+update();
